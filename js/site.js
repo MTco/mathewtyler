6af7979d3edcf13b4851feb711566df03fc64367
maxmillion <github@onlate.com>
2016-08-07 07:13:50 -0700

    Who has two thumbs and remembers the rudiments of his linear algebra courses?  Apparently, this guy.

--- js/site.js
+++ js/site.js
@@ -1,11 +1,13 @@
 'use strict';
-$(function(){
+(function($){
 	//document.getElementById(\'PersistentCookie\').checked=false;
 	var H=window,l=H.document,hash=location.hash,e,nIntervId,didInit=false,$el,tmp;
 	hash='#'+hash.substr(1,36);
 	function init(){
-		if(didInit===false){
-			if(typeof hash!=='undefined'&&hash!=='#'){
+		if(didInit===false)
+		{
+			if(typeof hash!=='undefined'&&hash!=='#')
+			{
 				clearInterval(nIntervId);
 				didInit=true;
 				$el=$('body',$('html')['0']).first().find(hash).first();
@@ -18,4 +20,4 @@ $(function(){
 	}
 	function v(){if(l.addEventListener){l.removeEventListener('DOMContentLoaded',v,false);init();}else{if(l.readyState==='complete'){l.detachEvent('onreadystatechange',v);init();}}}if(l.readyState==='complete'){init();}else{if(l.addEventListener){l.addEventListener('DOMContentLoaded',v,false);H.addEventListener('load',init,false);}else{l.attachEvent('onreadystatechange',v);H.attachEvent('onload',init);}}
 	nIntervId=setInterval(function(){var asdf=init();},600);
-});
+}(jQuery.noConflict(true)));
