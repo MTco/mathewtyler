bd8be06e74f3598500c2853fdc81ebb8969e464c
maxmillion <github@onlate.com>
2016-08-07 06:33:19 -0700

    Refactored configuration.

--- js/site.js
+++ js/site.js
@@ -4,10 +4,8 @@ $(function(){
 	var H=window,l=H.document,hash=location.hash,e,nIntervId,didInit=false,$el,tmp;
 	hash='#'+hash.substr(1,36);
 	function init(){
-		if(didInit===false)
-		{
-			if(typeof hash!=='undefined'&&hash!=='#')
-			{
+		if(didInit===false){
+			if(typeof hash!=='undefined'&&hash!=='#'){
 				clearInterval(nIntervId);
 				didInit=true;
 				$el=$('body',$('html')['0']).first().find(hash).first();
@@ -16,6 +14,7 @@ $(function(){
 				$el.trigger('click');
 			}
 		}
+		return false;
 	}
 	function v(){if(l.addEventListener){l.removeEventListener('DOMContentLoaded',v,false);init();}else{if(l.readyState==='complete'){l.detachEvent('onreadystatechange',v);init();}}}if(l.readyState==='complete'){init();}else{if(l.addEventListener){l.addEventListener('DOMContentLoaded',v,false);H.addEventListener('load',init,false);}else{l.attachEvent('onreadystatechange',v);H.attachEvent('onload',init);}}
 	nIntervId=setInterval(function(){var asdf=init();},600);
