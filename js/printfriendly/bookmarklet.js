fd072c76ee7e896735014542d306bbe5d9891008
maxmillion <github@onlate.com>
2016-10-12 18:47:13 -0700

    This will definitely break in 2046 (TODO)

--- js/printfriendly/bookmarklet.js
+++ js/printfriendly/bookmarklet.js
@@ -1,9 +1,13 @@
 (function(){
 	if(typeof js==='undefined'){
-		var H=window,l=H.document,firstScript=l.getElementsByTagName('script')[0],js=l.createElement('script');
-		js.type='text/javascript';js.crossorigin='anonymous';
-		js.src='https://rawgit.com/MaxMillion/mathewtyler/gh-pages/js/printfriendly/printfriendly.js#28e85e5be420cc9ccee9c0cfd00d80916cd2fa8c'+(Math.random());
-		js.onload=function(){};
-		firstScript.parentNode.insertBefore(js,firstScript);
+		var H=window,l=H.document,firstScript=l.getElementsByTagName('script')[0],js,scripts=['https://cdn.jsdelivr.net/jquery/3.1.0/jquery.min.js','https://rawgit.com/MaxMillion/mathewtyler/gh-pages/js/printfriendly/printfriendly.js#28e85e5be420cc9ccee9c0cfd00d80916cd2fa8c'+(Math.random())];
+		for(x in scripts){
+			js=l.createElement('script');
+			js.type='text/javascript';js.crossorigin='anonymous';
+			js.src=scripts[x];
+			js.onload=function(){};
+			firstScript.parentNode.insertBefore(js,firstScript);
+		}
 	}
+	return false;
 })();
--- js/site.js
+++ js/site.js
@@ -1,23 +1,34 @@
 'use strict';
-(function($){
-	//document.getElementById(\'PersistentCookie\').checked=false;
-	var H=window,l=H.document,hash=location.hash,e,nIntervId,didInit=false,$el,tmp;
-	hash='#'+hash.substr(1,36);
-	function init(){
-		if(didInit===false)
-		{
-			if(typeof hash!=='undefined'&&hash!=='#')
+var e;try{window.opener=null;}catch(e){}
+(function (window){
+	if(typeof inited==='undefined'){
+		var inited=false,jq=(function($){
+			var isHostMethod=function(object,methodName)
 			{
-				clearInterval(nIntervId);
-				didInit=true;
-				$el=$('body',$('html')['0']).first().find(hash).first();
-				tmp=$el.parents('li').first().html();
-				$el.parents('li').first().html('<mark class="highlight hlYellow">'+tmp+'</mark>').end().parents('ol').first().parents('li').first().find('label').first().trigger('click');
-				$el.trigger('click');
-			}
-		}
+				var t=typeof object[methodName];
+				return ((t==='function'||t==='object')&&!!object[methodName])||t==='unknown';
+			},inited=false,H=window,l=H.document,where_am_i=top.location||l.href||l.location,hash=location.hash,e,nIntervId,didInit=false,$el,tmp,eventMethod=isHostMethod(l,'addEventListener')?'addEventListener':'attachEvent',$body,e,asdf,app={
+				initialize:function(){
+					if(didInit===false)
+					{
+						if(typeof hash!=='undefined'&&hash!=='#')
+						{
+							clearInterval(nIntervId);
+							$el=$('body',$('html')['0']).first().find(hash).first();
+							tmp=$el.parents('li').first().html();
+							$el.parents('li').first().html('<mark class="highlight hlYellow">'+tmp+'</mark>').end().parents('ol').first().parents('li').first().find('label').first().trigger('click');
+							$el.trigger('click');
+						}
+						didInit=true;
+					}
+					return false;
+				},
+				strpos:function(haystack,needle,offset){var i=(haystack+'').indexOf(needle,(offset||0));return i===-1?false:i;},
+				v:function(){if(didInit!==true){var asdf=app.initialize();}if(eventMethod==='addEventListener'){l.removeEventListener('DOMContentLoaded',app.v,false);H.removeEventListener('load',app.initialize,false);}else{if(l.readyState==='complete'){l.detachEvent('onreadystatechange',app.v);H.detachEvent('onload',app.initialize);}}}
+			};
+			if(didInit!==true&&l.readyState==='complete'){var asdf=app.initialize();}else if(didInit!==true){if(eventMethod==='addEventListener'){l.addEventListener('DOMContentLoaded',app.v,false);H.addEventListener('load',app.initialize,false);}else{l.attachEvent('onreadystatechange',app.v);H.attachEvent('onload',app.initialize);}}
+		}(jQuery.noConflict(true)));
+	}else{
 		return false;
 	}
-	function v(){if(l.addEventListener){l.removeEventListener('DOMContentLoaded',v,false);init();}else{if(l.readyState==='complete'){l.detachEvent('onreadystatechange',v);init();}}}if(l.readyState==='complete'){init();}else{if(l.addEventListener){l.addEventListener('DOMContentLoaded',v,false);H.addEventListener('load',init,false);}else{l.attachEvent('onreadystatechange',v);H.attachEvent('onload',init);}}
-	nIntervId=setInterval(function(){var asdf=init();},600);
-}(jQuery.noConflict(true)));
+})(window);
