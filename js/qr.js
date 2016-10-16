f4b457531300fc91c39e7f5e7461097ff2255ad2
maxmillion <github@onlate.com>
2016-10-16 15:06:22 -0700

    Nitpicking about alphabetizing methods, minor OCD thing

---
+++ js/qr.js
@@ -0,0 +1,3 @@
+'use strict';
+var H=window,l=H.document,where_am_i=top.location||l.href||l.location;
+if(typeof where_am_i.hash==='undefined'||where_am_i.hash===''){where_am_i+='#qr';top.location=where_am_i;}
