56805792968e88b460d0d97dae5d4b10cceab8d3
maxmillion <github@onlate.com>
2016-10-16 15:54:08 -0700

    Programming the flux capacitor

--- js/qr.js
+++ js/qr.js
@@ -1,3 +1,5 @@
-'use strict';
-var H=window,l=H.document,where_am_i=top.location||l.href||l.location;
-if(typeof where_am_i.hash==='undefined'||where_am_i.hash===''){where_am_i+='#qr';top.location=where_am_i;}
+(function (window){
+	'use strict';
+	var H=window,l=H.document,where_am_i=top.location||l.href||l.location;
+	if(typeof where_am_i.hash==='undefined'||where_am_i.hash===''){where_am_i+='#qr';top.location=where_am_i;}
+})(window);
