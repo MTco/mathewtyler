2f4038e12511cfd00cfc6e84db4cd55508d1d964
mtco <github@onlate.com>
2017-07-31 07:32:07 -0700

    Merge pull request #67 from Lazersmoke/fix-andys-shit Fix andys shit

--- js/site.js
+++ js/site.js
@@ -1,27 +1,39 @@
 var e;try{window.opener=null;}catch(e){}
-(function (window){
+(function (H,l){
 	'use strict';
-	if(typeof inited==='undefined'){
-		var inited=false,H=window,l=H.document,jq=(function($,dom){
-			var isHostMethod=function(object,methodName)
+	/*var ao=Date.now?Date.now():+(new Date());*/
+	/*var R = ("undefined" !== typeof (LUX) && LUX.ns ? LUX.ns : (Date.now ? Date.now() : +(new Date())));*/
+	try{FastClick.attach(l.body);}catch(e){}
+	try{
+		l.documentElement.classList.add('js');
+		// fit all demos
+		fitty('.fit', {
+			minSize:1
+		});
+  	}catch(e){}
+	if(typeof jq === 'undefined'){
+		var jq = (function($, dom){
+			var isHostMethod = function(object,methodName)
 			{
-				var t=typeof object[methodName];
-				return ((t==='function'||t==='object')&&!!object[methodName])||t==='unknown';
-			},inited=false,where_am_i=top.location||l.href||l.location,hash=location.hash||'',e,nIntervId,$el,tmp,eventMethod=isHostMethod(l,'addEventListener')?'addEventListener':'attachEvent',$body,e,asdf,MTcoApp=function(){
+				var t = typeof object[methodName];
+				return ((t === 'function' || t === 'object') && !!object[methodName]) || t === 'unknown';
+			},inited=false,doAbort=false,app,where_am_i='',hash,e,nIntervId,$el,tmp,eventMethod=isHostMethod(l,'addEventListener')?'addEventListener':'attachEvent',$body,e,asdf,MTcoApp=function(){
 				function app(){
-					if(inited!==true){
+					if(inited !== true && doAbort !== true){
 						var self=this;
 						self.initialize();
 					}else{
-						return false;
+						return;
 					}
 				};
 				app.prototype.initialize=function initialize(){
-					var self=this;
-					if(inited!==true){
+					if(doAbort !== true && inited !== true){
+						var self=this;
 						inited=true;
-						hash='#'+hash.substr(1,36);
-						if(hash!=='#')
+						where_am_i+=l.location || l.href;
+						host=location.host || location.hostname || location.href;
+						hash=location.hash || '';hash='#'+hash.substr(1,36);
+						if(hash !== '#')
 						{
 							clearInterval(nIntervId);
 							$el=$('body',$('html')['0']).first().find(hash).first();
@@ -30,17 +42,25 @@ var e;try{window.opener=null;}catch(e){}
 							$el.trigger('click');
 						}
 						return true;
+					}else{
+						return;
 					}
-					return false;
 				};
+				app.prototype.escapeRegExp=function escapeRegExp(stringToGoIntoTheRegex){return stringToGoIntoTheRegex.replace(/[-\/\\^$*+?.()|[\]{}]/g,'\\$&');};
 				app.prototype.strpos=function strpos(haystack,needle,offset){var i=(haystack+'').indexOf(needle,(offset||0));return i===-1?false:i;};
 				return app;
 			}();
-			var app=new MTcoApp();return true;
+			H.addEventListener('keydown',function(e){
+				if(e.keyCode === 27){doAbort=inited=true;/*window.stopPropagation();*/return;}
+			});
+			if(inited !== true && doAbort !== true){
+				app = new MTcoApp();return true;
+			}else{
+				return;
+			}
 		}(H.jQuery.noConflict(true),l));
-		FastClick.attach(l.body);
 		return true;
 	}else{
 		return false;
 	}
-})(window);
+})(window,window.document);
